4bf60c63ceae17ca28ddf80e29a3c7def8775251
Luis Tejero <luisfaridta@gmail.com>
2018-05-07 11:36:22 +0100

    pre settings for create request done, updated links for requests on users/animals

--- app.rb
+++ app.rb
@@ -202,3 +202,27 @@ post "/users/new_user/created" do
   @user.save()
   erb(:"users/users/created", :layout => :user_layout)
 end
+
+
+# REQUEST
+# CREATE user animal Request
+
+get "/users/animals/:id/set_user" do
+  @animal = Animal.find(params[:id])
+  @types = Type_of_animal.all
+  @users = User.all
+  erb(:"users/requests/set_user", :layout => :user_layout)
+end
+
+get "/users/animals/:id/set_user/:userid/set_comment" do
+@animal = Animal.find(params[:id])
+@user = User.find(params[:userid])
+erb(:"users/requests/set_comment", :layout => :user_layout)
+end
+
+post "/users/animals/:id/set_user/:userid/set_comment/:comment/created" do
+@animal = Animal.find(params[:id])
+@user = Owner.find(params[:userid])
+@user.create_request(@animal, params[:comment])
+erb(:"users/requests/request_created", :layout => :user_layout)
+end
